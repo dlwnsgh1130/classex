c30a83ebd2ec2d32a35a1e5fa69e42f82bad16e9
dlwnsgh1130 <dlwnsgh1130@bu.ac.kr>
2026-03-30 21:50:52 +0900

    두번째 커밋

--- src/main/java/com/example/kdt20214026/classex/ShoppingCart.java
+++ src/main/java/com/example/kdt20214026/classex/ShoppingCart.java
@@ -20,7 +20,37 @@ public class ShoppingCart {
         System.out.println(sp.getName() + " " + qty + "개 담았습니다.");
     }
     // 삭제
-    public void removeItem(){
+    public void removeItem(String productName){
+        //리스트 안에 해당 상품이 있는지 탐색
+        //삭제
 
+        boolean removed = false;
+        /*
+        //리스트의 맨 끝 인덱스부터 탐색
+        for(int i = itemList.size() -1; i >= 0 ; i--){
+            CartItem item = itemList.get(i);
+            //상품평 비교
+            if(item.getSp().getName().equals(productName)){
+                itemList.remove(i);//조건이 맞으면 해당 인덱스 삭제
+
+                removed = true;
+            }
+        }
+
+
+        // forEach(); /탐색, 조회, 출력
+        itemList.forEach(item ->{
+            if(item.getSp().getName().equals(productName)){
+                //itemList.remove(i); 삭제시 오류
+            }
+        });
+        */
+
+        if(removed){
+            System.out.println("장바구니에서 삭제했습니다.");
+        }
+        else {
+            System.out.println(productName + "이 없습니다.");
+        }
     }
 }
--- src/main/java/com/example/kdt20214026/classex/ShoppingMain.java
+++ src/main/java/com/example/kdt20214026/classex/ShoppingMain.java
@@ -1,4 +1,22 @@
 package com.example.kdt20214026.classex;
 
 public class ShoppingMain {
+
+    public static void main(String[] args){
+        //상품 생성
+        ShoppingProduct apple = new ShoppingProduct("감홍",8000);
+        ShoppingProduct banana = new ShoppingProduct("필리핀",4000);
+        ShoppingProduct pear = new ShoppingProduct("탱크보이",1200);
+
+        //장바구니 생성 후 상품 닫기
+        ShoppingCart cart = new ShoppingCart();
+
+        cart.addItem(apple, 1);
+        cart.addItem(banana, 3);
+        cart.addItem(pear, 12);
+
+        cart.removeItem("감수광");
+        cart.removeItem("탱크보이");
+    }
+
 }
